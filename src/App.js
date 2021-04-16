563a0396242e92e1294e5d7a0975bbd727332a84
Sumbul Naz <sumbul.1304@gmail.com>
2021-04-16 13:50:34 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderOne from './header/HeaderOne'
+import HeaderTwo from './header/HeaderTwo'
+import HeaderThree from './header/HeaderThree'
+import HeaderFour from './header/HeaderFour'
+import SingleImageBody from './body/SingleImageBody'
+import SingleImageCenter from './body/SingleImageCenter'
+import Body from './body/Body'
+import PriceOne from './pricing/PriceOne'
+import PriceTwo from './pricing/PriceTwo'
+import StaticsOne from './statics/StaticsOne';
+import StaticsTwo from './statics/StaticsTwo';
+import StaticsThree from './statics/StaticsThree';
+import StepOne from './steps/StepOne';
+import StepTwo from './steps/StepTwo';
+import GridOne from './ecommerce/GridOne';
+import GridTwo from './ecommerce/GridTwo';
+import GridThree from './ecommerce/GridThree';
+import DetailsOne from './feature/DetailsOne';
+import DetailsTwo from './feature/DetailsTwo';
+import DetailsThree from './feature/DetailsThree';
+import DetailsList from './feature/DetailsList';
+import FooterOne from './footer/FooterOne';
+import FooterTwo from './footer/FooterTwo';
+import FooterThree from './footer/FooterThree';
+import GalleryOne from './gallery/GalleryOne';
+import GalleryTwo from './gallery/GalleryTwo';
+import GalleryThree from './gallery/GalleryThree';
+import TeamOne from './team/TeamOne';
+import TeamTwo from './team/TeamTwo';
+import TeamThree from './team/TeamThree';
+import ContactOne from './contact/ContactOne';
+import ContactTwo from './contact/ContactTwo';
+import ContentOne from './content/ContentOne';
+import ContentTwo from './content/ContentTwo';
+import TestimonialOne from './testimonial/TestimonialOne';
+import TestimonialTWo from './testimonial/TestimonialTwo';
+import Blog from './blog/Blog';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    {/* <HeaderOne/> */}
+    {/* <HeaderTwo/> */}
+    {/* <HeaderThree/> */}
+    {/* <HeaderFour/> */}
+    {/* <SingleImageBody/> */}
+    {/* <SingleImageCenter/> */}
+    <Body/>
+    {/* <PriceOne/> */}
+    {/* <PriceTwo/> */}
+    {/* <StaticsOne/> */}
+    {/* <StaticsTwo/> */}
+    {/* <StaticsThree/> */}
+    {/* <StepOne/> */}
+    {/* <StepTwo/> */}
+    {/* <GridOne/> */}
+    {/* <GridTwo/> */}
+    {/* <GridThree/> */}
+    {/* <DetailsOne/> */}
+    {/* <DetailsTwo/> */}
+    {/* <DetailsThree/> */}
+    {/* <DetailsList/> */}
+    {/* <FooterOne/> */}
+    {/* <FooterTwo/> */}
+    
+    {/* <GalleryOne/> */}
+    {/* <GalleryTwo/> */}
+    {/* <GalleryThree/> */}
+    {/* <TeamOne/> */}
+    {/* <TeamTwo/> */}
+    {/* <TeamThree/> */}
+    {/* <ContactOne/> */}
+    {/* <ContactTwo/> */}
+    {/* <ContentOne/> */}
+    {/* <ContentTwo/> */}
+    {/* <TestimonialOne/> */}
+    {/* <TestimonialTWo/> */}
+    {/* <Blog/> */}
+    {/* <FooterThree/> */}
     </div>
   );
 }
